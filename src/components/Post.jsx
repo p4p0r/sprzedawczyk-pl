64866b02fbb1a3ecea6bf4cad38465c9083a9fc3
PAPITO-18CM <maksczapor06@gmail.com>
2024-11-03 13:13:47 +0100

    dodawanie postów, wyświetlanie z db

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,36 @@
-import './post.css'
+import classes from './Post.module.css'
+import { Link } from 'react-router-dom'
 
-export function Post({title, location, price, image, desc}){
+export function Post({id, title, location, price, image, desc}){
+
+    function savePost(){
+        console.log(id)
+    }
+
     return(
-        <div className="post">
-            <div className="title">{title}, {location}</div>
-            <div className="image"><img src={image} alt={title}/></div>
-            <div className="desc">{desc}</div>
-            <div className="price">{price} PLN</div>
+        <div className={classes.post}>
+            <div className={classes.title}>
+                <Link to={`/post/${id}`}>
+                    {title}
+                </Link>,
+                {location}
+            </div>
+            
+            <div className={classes.image}>
+                <img src={`http://localhost:8000/${image}`} alt={title}/>
+            </div>
+            
+            <div className={classes.desc}>
+                {desc}
+            </div>
+            
+            <div className={classes.price}>
+                {price} PLN
+                <span className={classes.save} onClick={savePost}>
+                    Zapisz ogłoszenie
+                </span>
+            </div>
+
         </div>
     )
 }
